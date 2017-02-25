3298dc74b00b65aa78628c939d2c2ffa75160dbe
Choyon Ahmed <choyonbd.tk@gmail.com>
2017-02-25 13:03:02 +0600

    Add files via upload

---
+++ Domain To IP.py
@@ -0,0 +1,19 @@
+import re, urllib2
+headers = { 'User-Agent' : 'Mozilla/5.0' }
+fileopen = open('s.txt', 'r').readlines()
+for i in fileopen:
+    try:
+        i = i.replace('\n','')
+        if 'http://' in i:
+            i = i.replace('http://', '')
+        print 'Working on,', i
+        req = urllib2.Request('http://reverseip.domaintools.com/search/?q=' + i, None, headers)
+        html = urllib2.urlopen(req).read()
+        found = re.findall('<title>.[\w]+.[\w]+.[\w]+.[\w]+',html)
+        found[0] = found[0].replace('<title>','')
+        print 'IP is:', found[0]
+        open('IP_collected.txt', 'a+').write(found[0] + '\n')
+    except:
+        print 'Found error on', i
+        pass
+print 'All saved as IP_collected.txt'
